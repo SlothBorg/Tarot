7a933e94c0e9cf71a5f660d883a88aa77c3ddfe7
Slothborg <SlothBorg@users.noreply.github.com>
2022-02-08 21:44:16 -0500

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    @include('layouts.parts.header')
-</head>
 <body class="antialiased">
-
-<div class="relative flex justify-center min-h-screen bg-sufiGold">
-    <div class="max-w-7xl mix-w-xl ">
 {{--        <div class="p-4">--}}
 {{--            @if (Route::has('login'))--}}
 {{--                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">--}}
@@ -23,21 +17,36 @@
 {{--            @endif--}}
 {{--        </div>--}}
 
-        <div class="bg-gray-100 p-4 rounded-lg content-center">
-            <section class="flex flex-row">
-                <div class="w-1/2 px-3 mb-6">
-                    <h1 class="text-center">Deck</h1>
-                    <img src="{{ asset('img/back.jpg') }}">
-                </div>
-                <div class="w-1/2 px-3 mb-6">
-                    <h1 class="text-center">Card</h1>
-                    <img src="{{ asset('img/tarot-fool.jpg') }}">
-                </div>
-            </section>
+<div class="flex justify-center">
+
+    <section class="max-w-7xl flex justify-between items-center h-screen">
+
+        <div class="w-1/4 px-3 mb-6">
+            <h1 class="text-center">Deck</h1>
+            <img class="mx-auto" src="{{ asset('img/back.jpg') }}">
         </div>
-    </div>
+
+        <div class="w-1/4 px-3 mb-6">
+            <h1 class="text-center">Card</h1>
+            <img class="mx-auto" src="{{ asset('img/tarot-fool.jpg') }}">
+        </div>
+    </section>
 </div>
+
+
+{{--    <div class="max-w-7xl mix-w-xl ">--}}
+
+
+{{--        <div class="bg-gray-100 p-4 rounded-lg content-center">--}}
+{{--            <section class="flex flex-row">--}}
+
+{{--            </section>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 
 {{--    @include('layouts.parts.footer')--}}
 </body>
+<head>
+    @include('layouts.parts.header')
+</head>
 </html>
